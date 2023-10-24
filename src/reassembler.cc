243ec2f80995caedc473d8081a2502227db5b648
licoded <busy.li@foxmail.com>
2023-10-24 02:33:28 +0000

    opt(reassembler): deal with overlap before check `first_index == next_index`
    doing this can reduce redundant code logic

--- src/reassembler.cc
+++ src/reassembler.cc
@@ -20,57 +20,6 @@ void Reassembler::insert( uint64_t first_index, string data, bool is_last_substr
     }
   }
 
-  if ( first_index == next_index ) {
-    /**
-     * TODO: write to `Writer& output`
-     *
-     * ATTENTION: the maximum write size is limited by `Writer::available_capacity()`
-     * ATTENTION: after writing this incoming datagram, we should check and pop some datagrams in datagram_queue
-     */
-    // write to `Writer& output`
-    uint64_t true_write_size = min( data.size(), output.available_capacity() );
-    output.push( data.substr( 0, true_write_size ) );
-    next_index += true_write_size;
-
-    if ( is_last_substring && true_write_size == data.size() ) {
-      output.close();
-    }
-
-    // check and pop some datagrams in datagram_queue
-    while ( output.available_capacity() > 0 && !datagram_queue.empty() ) {
-      auto [index, datagram, is_end] = datagram_queue.top();
-      datagram_queue.pop();
-
-      if ( index < next_index ) {
-        if ( index + datagram.size() <= next_index ) { // 10, 10-19, 20
-          // do nothing, just discard this datagram if it is already received
-        } else {
-          datagram = datagram.substr( next_index - index );
-          output.push( datagram );
-          next_index += datagram.size();
-        }
-        temporary_bytes -= datagram.size();
-        continue;
-      } else if ( index == next_index ) {
-        /**
-         * NOTE: all datagrams in `datagram_queue` are in capacity
-         */
-        output.push( datagram );
-        if ( is_end ) {
-          output.close();
-        }
-        next_index += datagram.size();
-        temporary_bytes -= datagram.size();
-      } else // index > next_index
-      {
-        datagram_queue.push( tuple( index, datagram, is_end ) );
-        break;
-      } // END else
-    }   // END while
-
-    return;
-  }
-
   /**
    * judge if the `first_index` is in the range of `next_index` and `next_index + Writer::remaining_capacity()`
    * - If yes, store the data in `datagram_queue`
@@ -134,6 +83,32 @@ void Reassembler::insert( uint64_t first_index, string data, bool is_last_substr
       data = data + data_.substr( last_index + 1 - first_index_, last_index_ - last_index ); // 10, 10-19, 15
       last_index = last_index_;
     }
+  }
+
+  if ( first_index == next_index ) {
+    output.push( data );
+    next_index += data.size();
+
+    if ( is_last_substring )
+      output.close();
+
+    while ( !datagram_queue.empty() ) {
+      auto [first_index_, data_, is_last_substring_] = datagram_queue.top();
+
+      if ( first_index_ > next_index )
+        break;
+      datagram_queue.pop();
+
+      output.push( data_ );
+      next_index += data_.size();
+
+      if ( is_last_substring_ )
+        output.close();
+
+      temporary_bytes -= data_.size();
+    }
+
+    return;
   }
 
   datagram_queue.push( make_tuple( first_index, data, is_last_substring ) );
