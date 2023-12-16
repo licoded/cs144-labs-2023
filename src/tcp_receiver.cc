cfd7c14862b7ac10ed24afadab7a13dd7d0114ab
licoded <busy.li@foxmail.com>
2023-12-16 22:48:56 +0800

    lab2: tcp_receiver try-2st, failed on recv_window test

--- src/tcp_receiver.cc
+++ src/tcp_receiver.cc
@@ -9,13 +9,16 @@ void TCPReceiver::receive( TCPSenderMessage message, Reassembler& reassembler, W
   // (void)reassembler;
   // (void)inbound_stream;
 
-  if ( message.SYN ) {
+  if ( !initialized && message.SYN ) {
     initialized = true;
     zero_point = message.seqno;
   }
 
-  uint64_t check_point = inbound_stream.bytes_pushed() + inbound_stream.bytes_pushed();
+  uint64_t check_point = inbound_stream.bytes_pushed();
   uint64_t first_index = message.seqno.unwrap( zero_point, check_point );
+
+  if (first_index > 0)
+    first_index -= 1;
 
   reassembler.insert( first_index, message.payload, message.FIN, inbound_stream );
 }
@@ -25,12 +28,12 @@ TCPReceiverMessage TCPReceiver::send( const Writer& inbound_stream ) const
   // Your code here.
   // (void)inbound_stream;
 
-  uint16_t window_size = inbound_stream.available_capacity();
+  uint16_t window_size = min( uint64_t(UINT16_MAX) ,inbound_stream.available_capacity() );
 
   if ( !initialized ) {
     return TCPReceiverMessage( {}, window_size );
   }
 
-  Wrap32 ackno = zero_point + inbound_stream.bytes_pushed() + 1;
+  Wrap32 ackno = zero_point + inbound_stream.bytes_pushed() + 1 + inbound_stream.is_closed();
   return TCPReceiverMessage( ackno, window_size );
 }
