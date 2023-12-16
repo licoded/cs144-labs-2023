e65e53f15e3e704d8c36d1eb62bf0d75395edbca
licoded <busy.li@foxmail.com>
2023-12-16 22:34:32 +0800

    lab2: finish tcp_receiver try-1st

--- src/tcp_receiver.cc
+++ src/tcp_receiver.cc
@@ -5,14 +5,32 @@ using namespace std;
 void TCPReceiver::receive( TCPSenderMessage message, Reassembler& reassembler, Writer& inbound_stream )
 {
   // Your code here.
-  (void)message;
-  (void)reassembler;
-  (void)inbound_stream;
+  // (void)message;
+  // (void)reassembler;
+  // (void)inbound_stream;
+
+  if ( message.SYN ) {
+    initialized = true;
+    zero_point = message.seqno;
+  }
+
+  uint64_t check_point = inbound_stream.bytes_pushed() + inbound_stream.bytes_pushed();
+  uint64_t first_index = message.seqno.unwrap( zero_point, check_point );
+
+  reassembler.insert( first_index, message.payload, message.FIN, inbound_stream );
 }
 
 TCPReceiverMessage TCPReceiver::send( const Writer& inbound_stream ) const
 {
   // Your code here.
-  (void)inbound_stream;
-  return {};
+  // (void)inbound_stream;
+
+  uint16_t window_size = inbound_stream.available_capacity();
+
+  if ( !initialized ) {
+    return TCPReceiverMessage( {}, window_size );
+  }
+
+  Wrap32 ackno = zero_point + inbound_stream.bytes_pushed() + 1;
+  return TCPReceiverMessage( ackno, window_size );
 }
